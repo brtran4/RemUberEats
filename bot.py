a102a4eb2559bf5f7288671527e68bf3d4b4170d
Brian Tran <brtran@umich.edu>
2024-02-01 03:43:11 -0500

    Implement unclaimed/claimed orders
    This commit implements two major commands: unclaimed and claimed
    orders.
    
    unclaimed_orders shows which orders have been completed but not claimed
    while claimed orders will erase the "now" claimed orders from unclaimed.txt
    
    This commit also cleans up reading/writing files to be more generic instead
    of writing to the general orders.txt

--- bot.py
+++ bot.py
@@ -12,20 +12,20 @@ client = discord.Client(intents=intents)
 tree = app_commands.CommandTree(client)
 
 
-def write_to_file(orders):
-    with open("orders.txt", "w") as file:
+def write_to_file(file, orders):
+    with open(file, "w") as file:
         for order in orders:
             file.write(json.dumps(order) + "\n")
 
 
-def read_from_file():
+def read_from_file(input_file):
     orders = []
     try:
-        with open("orders.txt", "r") as file:
+        with open(input_file, "r") as file:
             for order in file:
                 orders.append(json.loads(order))
     except:
-        with open("orders.txt", "w") as file:
+        with open(input_file, "w") as file:
             file.write("")
     return orders
 
@@ -34,7 +34,7 @@ def read_from_file():
 @app_commands.describe(item="The item you want to buy")
 @app_commands.describe(quantity="The quantity of the item you want to buy")
 async def order(interaction: Interaction, item: str, quantity: int):
-    orders = read_from_file()
+    orders = read_from_file("orders.txt")
     order = {
         "order": item,
         "quantity": quantity,
@@ -48,18 +48,18 @@ async def order(interaction: Interaction, item: str, quantity: int):
         await interaction.response.send_message(f"Arigatogozaimasu {interaction.user.global_name} senpai! I will ping you when he's done cooking desu~")
     else:
         await interaction.response.send_message("Order received! I'll send this over to Rem and I will ping you when he's done. Thank you for your patronage!")
-    write_to_file(orders)
+    write_to_file("orders.txt", orders)
     
 
 @tree.command(name="show_orders", description="Show all currently open orders", guild=discord.Object(SERVER_ID))
 async def show_orders(interaction: Interaction): 
     embed=discord.Embed(
-    title="Current Orders",
+        title="Current Orders",
         description="Here are the currently open orders:",
         color=Color.blue()
     )
 
-    orders = read_from_file()
+    orders = read_from_file("orders.txt")
     if len(orders) == 0:
         embed.add_field(name="", value="There are currently no orders.")
 
@@ -75,20 +75,70 @@ async def show_orders(interaction: Interaction):
 @tree.command(name="complete_order", description="Complete an order", guild=discord.Object(SERVER_ID))
 @app_commands.describe(order_num="Order number to complete")
 async def complete_order(interaction: Interaction, order_num: int):
+    unclaimed_orders = read_from_file("unclaimed.txt")
     if interaction.user.name != "remengis":
         await interaction.response.send_message("Hmmm... You're not Rem... Who are you? Sorry, you can't use this command!")
         return
 
-    orders = read_from_file()
+    orders = read_from_file("orders.txt")
     if order_num >= len(orders):
         await interaction.response.send_message("This order doesn't exist. Try again.")
         return
 
     order = orders[order_num]
     del orders[order_num]
-    write_to_file(orders)
 
-    await interaction.response.send_message(f'<@{order["customer_id"]}> Ding! Your order is ready!')
+    unclaimed_orders.append(order)
+    write_to_file("orders.txt", orders)
+    write_to_file("unclaimed.txt", unclaimed_orders)
+
+    await interaction.response.send_message(f'<@{order["customer_id"]}> Ding! Your order is ready! Please make sure to use `/claim` when receiving your product from Rem!')
+
+
+@tree.command(name="unclaimed_orders", description="Show your unclaimed orders", guild=discord.Object(SERVER_ID))
+async def show_unclaimed_orders(interaction: Interaction):
+    unclaimed_orders = []
+    all_unclaimed_orders = read_from_file("unclaimed.txt")
+
+    embed=discord.Embed(
+        title="Unclaimed Orders",
+        description="Here are your unclaimed orders:",
+        color=Color.red()
+    )
+
+    if len(all_unclaimed_orders) == 0:
+        embed.add_field(name="", value="There are currently no unclaimed orders.")
+
+    if interaction.user.name != "remengis":
+        unclaimed_orders = [unclaimed for unclaimed in all_unclaimed_orders if unclaimed["discord_name"] == interaction.user.name]
+        for order in unclaimed_orders:
+            val = f'{order["order"]} \({order["quantity"]}\)'
+            embed.add_field(name="", value=val, inline=False)
+    else:
+        counter = 0
+        for order in all_unclaimed_orders:
+            val = f'\#{counter}: {order["order"]} \({order["quantity"]}\) ordered by {order["customer"]}'
+            counter += 1
+            embed.add_field(name="", value=val, inline=False)
+
+    await interaction.response.send_message(embed=embed)
+
+
+@tree.command(name="claim_all_orders", description="Claim all of your completed orders", guild=discord.Object(SERVER_ID))
+async def claim_all_orders(interaction: Interaction):
+    claimed_orders = []
+    all_unclaimed_orders = read_from_file("unclaimed.txt")
+    claimed_orders = [unclaimed for unclaimed in all_unclaimed_orders if unclaimed["discord_name"] == interaction.user.name]
+
+    if len(claimed_orders) == 0:
+        await interaction.response.send_message("You currently don't have any completed orders to claim.")
+        return
+
+    unclaimed_orders = filter(lambda order: order not in claimed_orders, all_unclaimed_orders)
+
+    write_to_file("unclaimed.txt", unclaimed_orders)
+
+    await interaction.response.send_message("Thank you for using Rem Uber Eats! We look forward to serving you again soon!")
 
 
 
